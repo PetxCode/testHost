6e94a7c4aee829c6bce6e32c82053134f20c7e4d
PetxCode <71887594+PetxCode@users.noreply.github.com>
2022-06-03 07:42:52 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const url = process.env.URL;
+const mainURL = "mongodb+srv://classBuild:classBuild@cluster0.75kqx.mongodb.net/classBuildTest?retryWrites=true&w=majority
 
-mongoose.connect(url).then(() => {
+mongoose.connect(mainURL).then(() => {
 	console.log("Database is now connected...!");
 });
 
